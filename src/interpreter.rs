1102712761a512099f1f77814cc37d2e7e7247b0
André Ligné <andre@ligne.io>
2022-07-21 14:31:18 +0200

    Add subtract to the standard library

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -26,26 +26,7 @@ impl Interpreter {
     ) -> Result<Value, Error> {
         match &name[..] {
             "add" => {
-                let values = arguments.iter().map(|argument| self.evaluate(argument));
-                let mut numbers: Vec<i64> = vec![];
-
-                for value in values {
-                    match value {
-                        Ok(Value::Number(number)) => numbers.push(number),
-
-                        Ok(value) => {
-                            return Err(Error::new(
-                                "add requires all arguments to be Numbers",
-                                ErrorType::TypeError {
-                                    expected_type: crate::Type::Number,
-                                    actual_type: value.value_type(),
-                                },
-                            ));
-                        }
-
-                        Err(error) => return Err(error),
-                    }
-                }
+                let numbers = self.number_arguments(arguments)?;
 
                 if numbers.len() == 0 {
                     return Err(Error::new("Too few arguments", ErrorType::TooFewArguments));
@@ -54,10 +35,47 @@ impl Interpreter {
                 return Ok(Value::Number(numbers.iter().sum()));
             }
 
+            "subtract" => {
+                let numbers = self.number_arguments(arguments)?;
+
+                if numbers.len() == 0 {
+                    return Err(Error::new("Too few arguments", ErrorType::TooFewArguments));
+                }
+
+                return Ok(Value::Number(
+                    numbers.into_iter().reduce(|acc, a| acc - a).unwrap_or(0),
+                ));
+            }
+
             _ => Err(Error::new(
                 "Undefined function",
                 ErrorType::UndefinedFunction(name.to_string()),
             )),
         }
+    }
+
+    fn number_arguments(&self, arguments: &Vec<ASTNode>) -> Result<Vec<i64>, Error> {
+        let values = arguments.iter().map(|argument| self.evaluate(argument));
+        let mut numbers: Vec<i64> = vec![];
+
+        for value in values {
+            match value {
+                Ok(Value::Number(number)) => numbers.push(number),
+
+                Ok(value) => {
+                    return Err(Error::new(
+                        "add requires all arguments to be Numbers",
+                        ErrorType::TypeError {
+                            expected_type: crate::Type::Number,
+                            actual_type: value.value_type(),
+                        },
+                    ));
+                }
+
+                Err(error) => return Err(error),
+            }
+        }
+
+        return Ok(numbers);
     }
 }
